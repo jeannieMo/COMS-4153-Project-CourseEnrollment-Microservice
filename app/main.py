32110a302e4b195b57dde72ccf5cf9173302f63a
emanuelaromano98 <emauelaromano1998@gmail.com>
2024-12-06 03:24:13 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import students, courses
+from app.routers import students, courses
 
 """
     Entrypoint to the Course Enrollment interface! 
