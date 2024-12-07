c9bc0948dbb2eced4542f0075de17691285396cd
jeannieMo <135667525+jeannieMo@users.noreply.github.com>
2024-12-06 19:28:01 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routers import students, courses
 
 """
+
+    The wizards was here.
+
+
+    
     Entrypoint to the Course Enrollment interface! 
     For examples on what you can do with this, try running this `main.py` then go to your browser and do:
     
