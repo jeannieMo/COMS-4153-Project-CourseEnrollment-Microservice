d70e85da899484462a747bb18e13b23b04ffaaa2
jeannieMo <135667525+jeannieMo@users.noreply.github.com>
2024-12-10 14:49:34 -0500

    Update students.py correlation ID

--- app/routers/students.py
+++ app/routers/students.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Header
 from app.services.courseworks_api import CourseWorksAPI
-
+import uuid
 """
     Router in charge of connecting with `courseworks_api.py` to get the courses a student is enrolled in.
 """
@@ -10,6 +10,8 @@ router = APIRouter()
 # Fetch courses for a student by student_id (uni)
 @router.get("/users/{student_id}/courses", tags=["students"])
 async def get_student_courses(student_id: str, token: str = Header(...)):
+    correlation_id = request.headers.get("X-Correlation-ID", str(uuid.uuid4()))
+    print(f"Correlation ID: {correlation_id} - Fetching students for course: {course_code}")
     api = CourseWorksAPI(token)
     try:
         courses = api.get_student_courses(student_id)
